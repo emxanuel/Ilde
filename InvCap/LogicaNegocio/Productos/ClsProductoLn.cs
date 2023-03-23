78d57cf9e32fe89904bd19b746eec871491b70fa
emxanuel <ealg0528@gmail.com>
2023-03-22 21:33:56 -0400

    2

--- InvCap/LogicaNegocio/Productos/ClsProductoLn.cs
+++ InvCap/LogicaNegocio/Productos/ClsProductoLn.cs
@@ -23,7 +23,7 @@ namespace LogicaNegocio.Productos
             ObjDataBase = new ClsDataBase()
             {
                 NombreTabla = "Producto$",
-                NombreSP = "[dbo].[sp_Usuarios_Index]",
+                NombreSP = "[dbo].[so_Producto_Index]",
                 Scalar = false
             };
             Ejecutar(ref objProducto);
@@ -35,16 +35,17 @@ namespace LogicaNegocio.Productos
         {
             ObjDataBase = new ClsDataBase()
             {
-                NombreTabla = "Usuario",
-                NombreSP = "[dbo].[SP_Usuarios_Create]",
+                NombreTabla = "Producto$",
+                NombreSP = "[dbo].[SP_Producto_Create]",
                 Scalar = true
             };
 
-            ObjDataBase.DtParametros.Rows.Add(@"@NombreCompleto", "18", objProducto.NombreCompleto);
-            ObjDataBase.DtParametros.Rows.Add(@"@NombreUsuario", "18", objProducto.NombreUsuario);
-            ObjDataBase.DtParametros.Rows.Add(@"@Clave", "18", objProducto.Clave);
-            ObjDataBase.DtParametros.Rows.Add(@"@IdPermisos", "4", objProducto.IdPermisos);
-            ObjDataBase.DtParametros.Rows.Add(@"@Estado", "1", objProducto.Estado);
+            ObjDataBase.DtParametros.Rows.Add(@"@Descripcion", "18", objProducto.Descripcion);
+            ObjDataBase.DtParametros.Rows.Add(@"@Categoria", "18", objProducto.Categoria);
+            ObjDataBase.DtParametros.Rows.Add(@"@Almacen", "18", objProducto.Almacen);
+            ObjDataBase.DtParametros.Rows.Add(@"@PrecioCompra", "10", objProducto.PrecioCompra);
+            ObjDataBase.DtParametros.Rows.Add(@"@PrecioVenta", "10", objProducto.PredcioVenta);
+            ObjDataBase.DtParametros.Rows.Add(@"@Stock", "10", objProducto.Stock);
 
             Ejecutar(ref objProducto);
         }
@@ -53,11 +54,11 @@ namespace LogicaNegocio.Productos
         {
             ObjDataBase = new ClsDataBase()
             {
-                NombreTabla = "Usuario",
-                NombreSP = "[dbo].[sp_Usuarios_Read]",
+                NombreTabla = "Producto$",
+                NombreSP = "[dbo].[sp_Producto_Read]",
                 Scalar = false
             };
-            ObjDataBase.DtParametros.Rows.Add(@"@IdUsuario", "4", objProducto.IdUsuario);
+            ObjDataBase.DtParametros.Rows.Add(@"@IdProducto", "4", objProducto.Id);
             Ejecutar(ref objProducto);
         }
 
@@ -65,16 +66,16 @@ namespace LogicaNegocio.Productos
         {
             ObjDataBase = new ClsDataBase()
             {
-                NombreTabla = "Usuario",
-                NombreSP = "[dbo].[sp_Usuarios_Update]",
+                NombreTabla = "Producto$",
+                NombreSP = "[dbo].[sp_Producto_Update]",
                 Scalar = true
             };
-            ObjDataBase.DtParametros.Rows.Add(@"@IdUsuario", "4", objProducto.IdUsuario);
-            ObjDataBase.DtParametros.Rows.Add(@"@NombreCompleto", "18", objProducto.NombreCompleto);
-            ObjDataBase.DtParametros.Rows.Add(@"@NombreUsuario", "18", objProducto.NombreUsuario);
-            ObjDataBase.DtParametros.Rows.Add(@"@Clave", "18", objProducto.Clave);
-            ObjDataBase.DtParametros.Rows.Add(@"@IdPermisos", "4", objProducto.IdPermisos);
-            ObjDataBase.DtParametros.Rows.Add(@"@Estado", "1", objProducto.Estado);
+            ObjDataBase.DtParametros.Rows.Add(@"@Descripcion", "18", objProducto.Descripcion);
+            ObjDataBase.DtParametros.Rows.Add(@"@Categoria", "18", objProducto.Categoria);
+            ObjDataBase.DtParametros.Rows.Add(@"@Almacen", "18", objProducto.Almacen);
+            ObjDataBase.DtParametros.Rows.Add(@"@PrecioCompra", "10", objProducto.PrecioCompra);
+            ObjDataBase.DtParametros.Rows.Add(@"@PrecioVenta", "10", objProducto.PredcioVenta);
+            ObjDataBase.DtParametros.Rows.Add(@"@Stock", "10", objProducto.Stock);
 
             Ejecutar(ref objProducto);
         }
@@ -84,46 +85,11 @@ namespace LogicaNegocio.Productos
             ObjDataBase = new ClsDataBase()
             {
                 NombreTabla = "Usuario",
-                NombreSP = "[dbo].[sp_Usuarios_Delete]",
+                NombreSP = "[dbo].[sp_Productos_Delete]",
                 Scalar = true
             };
-            ObjDataBase.DtParametros.Rows.Add(@"@IdUsuario", "4", objProducto.IdUsuario);
+            ObjDataBase.DtParametros.Rows.Add(@"@IdProducto", "4", objProducto.Id);
             Ejecutar(ref objProducto);
-        }
-
-        public bool Login(string usuario, string clave)
-        {
-            SqlConnection conn = new SqlConnection("Data Source=DESKTOP-PPTKEH7\\MSSQLSERVER01;Initial Catalog=InventarioIlde;Integrated Security=True");
-            conn.Open();
-            string query = "select count(*) from usuario where nombreusuario = '" + usuario + "' and clave= '" + clave + "'";
-            SqlCommand cmd = new SqlCommand(query, conn);
-            SqlDataReader reader = cmd.ExecuteReader();
-            reader.Read();
-            int count = int.Parse(reader[0].ToString());
-            conn.Close();
-
-            return count == 1;
-        }
-
-        public ClsUsuario createUserObject(string usuario, string clave)
-        {
-            SqlConnection conn = new SqlConnection("Data Source=DESKTOP-PPTKEH7\\MSSQLSERVER01;Initial Catalog=InventarioIlde;Integrated Security=True");
-            conn.Open();
-            string query = "select NombreCompleto, NombreUsuario, Clave, IdPermisos, Estado from usuario where nombreusuario = '" + usuario + "' and clave= '" + clave + "'";
-            SqlCommand cmd = new SqlCommand(query, conn);
-            SqlDataReader reader = cmd.ExecuteReader();
-            reader.Read();
-            ClsProducto objProducto = new ClsUsuario()
-            {
-                NombreCompleto = reader[0].ToString(),
-                NombreUsuario = reader[1].ToString(),
-                Clave = reader[2].ToString(),
-                IdPermisos = int.Parse(reader[3].ToString()),
-                Estado = Convert.ToBoolean(reader[4])
-            };
-            conn.Close();
-
-            return objProducto;
         }
 
 
